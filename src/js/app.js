b0ed02de558c6fc7e42ddf2031a22577ac0def47
stefano capra <ste.capra95@gmail.com>
2019-01-30 13:46:16 +0100

    Add webpack and add .gitignore

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+// CSS
+
+require('../scss/main.scss')
+
+// JAVASCRIPT
+
+window.$ = require('jquery')
+
+require('foundation-sites')
+require('particles.js')
+require('wowjs')
+
+require('./main.js')
+
+
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        }]
+    }
+}
